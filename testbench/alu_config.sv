3cc1e9cd18735b2f8e990bb9e1c242e377318777
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-08-14 10:46:29 +0530

    Add output_agent_is_active to alu_config

--- testbench/alu_config.sv
+++ testbench/alu_config.sv
@@ -4,9 +4,7 @@ class alu_config extends uvm_object;
   virtual alu_if vif;
 
   uvm_active_passive_enum is_active=UVM_ACTIVE;
- 
-
-  
+  uvm_active_passive_enum output_agent_is_active=UVM_PASSIVE;
   function new(string name="alu_config");
 	super.new(name);
   endfunction
