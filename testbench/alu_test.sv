c22520873b19ef921e7c26b0f3b354da1ecf2cf2
KrishnaGirish2 <krishnagirish1458@gmail.com>
2026-08-14 10:44:32 +0530

    Refactor ALU test class for improved readability

--- testbench/alu_test.sv
+++ testbench/alu_test.sv
@@ -1,23 +1,22 @@
-
 class test extends uvm_test;
-	`uvm_component_utils(test)
+        `uvm_component_utils(test)
 
  env env_h;
  alu_config m_cfg;
 
  function new(string name="test",uvm_component parent);
-	super.new(name,parent);
+        super.new(name,parent);
  endfunction
 
  function void build_phase(uvm_phase phase);
-	super.build_phase(phase);
+        super.build_phase(phase);
 
   m_cfg=alu_config::type_id::create("m_cfg");
- 
-  if(!uvm_config_db#(virtual alu_if)::get(this,"","alu_if",m_cfg.vif))
-	`uvm_fatal(get_type_name(),"Can't get the interface")
-  m_cfg.is_active=UVM_ACTIVE; 
 
+  if(!uvm_config_db#(virtual alu_if)::get(this,"","alu_if",m_cfg.vif))
+        `uvm_fatal(get_type_name(),"Can't get the interface")
+  m_cfg.is_active=UVM_ACTIVE;
+m_cfg.output_agent_is_active=UVM_PASSIVE;
   uvm_config_db#(alu_config)::set(this,"*","alu_config",m_cfg);
 
   env_h=env::type_id::create("env_h",this);
